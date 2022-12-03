7e8d61fdab311b8ff0e13fdced5bbb25f0a93dbe
MathisHumbert <mathishumbert71@gmail.com>
2022-12-03 15:21:45 +0100

    added animation between menu and content

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 import gsap from 'gsap';
 
 const openButton = document.querySelector('.content__button__enter');
+const closeButton = document.querySelector('.frame__button__menu');
+
 const overlayPath = document.querySelector('.overlay__path');
 
 const contentTitleMain = document.querySelector('.content__title__main');
@@ -11,10 +13,10 @@ const frame = document.querySelector('.frame');
 const menuWrapper = document.querySelector('.menu__wrapper');
 const menuItems = document.querySelectorAll('.menu__item');
 
-const onEnter = () => {
-  const tl = gsap.timeline();
+let isMenuOpen = false;
 
-  console.log('click');
+const onEnter = () => {
+  const tl = gsap.timeline({ onComplete: () => (isMenuOpen = true) });
 
   tl.set(overlayPath, {
     attr: { d: 'M 0 100 V 100 Q 50 100 100 100 V 100 z' },
@@ -65,4 +67,56 @@ const onEnter = () => {
     );
 };
 
+const onLeave = () => {
+  if (!isMenuOpen) return;
+  const tl = gsap.timeline({ onComplete: () => (isMenuOpen = false) });
+
+  tl.set(overlayPath, {
+    attr: { d: 'M 0 0 V 0 Q 50 0 100 0 V 0 z' },
+  })
+    .to(
+      overlayPath,
+      {
+        attr: { d: 'M 0 0 V 50 Q 50 100 100 50 V 0 z' },
+        duration: 0.8,
+        ease: 'power4.in',
+      },
+      0
+    )
+    .to(overlayPath, {
+      attr: { d: 'M 0 0 V 100 Q 50 100 100 100 V 0 z' },
+      duration: 0.3,
+      ease: 'power2',
+      onComplete: () => {
+        frame.classList.remove('menu--open');
+        menuWrapper.classList.remove('menu--open');
+      },
+    })
+    .set(overlayPath, {
+      attr: { d: 'M 0 100 V 0 Q 50 0 100 0 V 100 z' },
+    })
+    .to(overlayPath, {
+      attr: { d: 'M 0 100 V 50 Q 50 100 100 50 V 100 z' },
+      duration: 0.3,
+      ease: 'power4.in',
+    })
+    .to(overlayPath, {
+      duration: 0.8,
+      ease: 'power4',
+      attr: { d: 'M 0 100 V 100 Q 50 100 100 100 V 100 z' },
+    })
+    .to(
+      [contentTitleMain, contentTitleSub],
+      { y: 0, duration: 1.1, ease: 'power4', stagger: -0.05 },
+      '>-=1.1'
+    )
+    .to(
+      menuItems,
+      { opacity: 0, y: 100, duration: 0.8, stagger: -0.05, ease: 'power2.in' },
+      0
+    );
+};
+
 openButton.addEventListener('click', onEnter);
+
+closeButton.addEventListener('click', onLeave);
